a50156bb641f819151111215bc27d055d01ebf98
mrandred <mrandrex1337@gmail.com>
2023-11-13 20:03:03 +0100

    Created platformer

---
+++ Assets/2D Platformer/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Transform[] _points;
+
+    [SerializeField] private Transform _path;
+    [SerializeField] private float _speed;
+
+    private Transform _target;
+    private int _currentPointIndex;
+    private float _flipAngle;
+    private float _defaultRotationY;
+
+    private void OnValidate()
+    {
+        _animator = GetComponent<Animator>();
+
+        _points = new Transform[_path.childCount];
+
+        for (int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+    }
+
+    private void Start()
+    {
+        _target = _points[0];
+        _defaultRotationY = 0;
+        _flipAngle = 180f;
+    }
+
+    private void Update()
+    {
+        MoveAuto();
+    }
+
+    private void MoveAuto()
+    {
+        Vector2 direction = (_target.position - transform.position).normalized;
+        float horizontal = direction.x * _speed * Time.deltaTime;
+
+        if (horizontal < _defaultRotationY)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, _flipAngle, transform.rotation.z);
+        }
+        else if (horizontal > _defaultRotationY)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, _defaultRotationY, transform.rotation.z);
+        }
+
+        if (Vector2.Distance(transform.position, _target.position) < 0.001f)
+        {
+            _currentPointIndex++;
+
+            if (_currentPointIndex >= _points.Length)
+            {
+                _currentPointIndex = 0;
+            }
+
+            _target = _points[_currentPointIndex];
+        }
+
+        float horizontalAbs = Mathf.Abs(horizontal);
+        _animator.SetFloat("Speed", horizontalAbs);
+        transform.Translate(horizontalAbs, 0, 0);
+    }
+}
---
+++ Assets/2D Platformer/Scripts/Player.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _jumpForce = 0.2f;
+    [SerializeField] private float _groundCheckDistance = 0.1f;
+
+    private bool _isGrounded;
+    private bool _isJumping;
+    private float _minSpeed;
+    private float _flipAngle;
+    private float _defaultRotationY;
+
+    private void Start()
+    {
+        _minSpeed = 0f;
+        _defaultRotationY = transform.rotation.y;
+        _flipAngle = 180f;
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleJump();
+    }
+
+    private void OnValidate()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void HandleInput()
+    {
+        if (_isGrounded == false)
+        {
+            _animator.SetFloat("Speed", _minSpeed);
+            return;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal") * _speed;
+
+        if (horizontal < _defaultRotationY)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, _flipAngle, transform.rotation.z);
+        }
+        else if (horizontal > _defaultRotationY)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, _defaultRotationY, transform.rotation.z);
+        }
+
+        _animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        _rigidbody2D.velocity = new Vector2(horizontal, _rigidbody2D.velocity.y);
+    }
+
+    private void HandleJump()
+    {
+        float jump = Input.GetAxis("Jump") * _jumpForce;
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, _groundCheckDistance);
+
+        if (jump > 0 && _isGrounded == true)
+        {
+            _rigidbody2D.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            _isJumping = true;
+        }
+
+        if (_isJumping == true)
+        {
+            _animator.SetTrigger("Jump");
+            _isJumping = false;
+        }
+    }
+}
